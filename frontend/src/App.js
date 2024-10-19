fa339eb641f420bae21e16c76886c903a61c5e49
Sharanya Jegadeesan <sharanyajegadeesan1999@gmail.com>
2024-10-19 07:58:55 +0530

    design the signin,signup page and setup backend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,8 @@
 import './App.css';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
+import SignIn from './pages/SignIn';
+import SignUp from './pages/SignUp';
 
 
 function App() {
@@ -8,6 +10,8 @@ function App() {
     <BrowserRouter>
     <Routes>
       <Route path='/' element={<Home/>}/>
+      <Route path='/signin' element={<SignIn/>}/>
+      <Route path='/signup' element={<SignUp/>}/>
     </Routes>
     
     </BrowserRouter>
